02ff68188737b4c40d275621eabd0dfa066e810c
Vishnu P <vinventor1999@gmail.com>
2024-04-23 12:55:35 +0530

    Basic setup done,components created and containers created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+//Importing components
+// import Article from "./components/article/Article.jsx";
+// import Brand from "./components/brand/Brand.jsx";
+// import CTA from "./components/cta/CTA.jsx";
+// import Feature from "./components/feature/Feature.jsx";
+// import Navbar from "./components/navbar/Navbar.jsx";
+import { Brand,CTA,Navbar } from './components';
+import { Blog,Features,Footer,Header,Possiblity,WhatGPT3} from "./containers";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possiblity />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/containers/possiblity/Possibilty.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./possiblity.css";
+
+const Possibilty = () => {
+  return (
+    <div>Possibilty</div>
+  )
+}
+
+export default Possibilty
